b4b9d34bc631aed2d03c6ef6e580117493a494af
Mishal Rai <minkumarrai@gmail.com>
2018-12-19 10:37:01 +0545

    Update webpack.config.js
    relative path option added on file-loader (webpack config file)

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,7 @@ module.exports = {
                     loader: 'file-loader',
                     options: {
                         name: '[name].[ext]',
+                        useRelativePath: true,
                         publicPath: '../img/'
                     }
                   }
