17d0ab7e3738500f8725d018fa2a6d04387da9a8
Mishal <raimuna145@gmail.com>
2019-01-30 21:29:43 +0545

    design system updated

--- template-parts/side_bar_menu.php
+++ template-parts/side_bar_menu.php
@@ -6,7 +6,7 @@
 
     <!-- SIDE MENU -->
     <div class="md-side-menu">
-        <a href="<?php get_home_url(); ?>" class="site-logo bg-frame-primary">
+        <a href="<?php get_home_url(); ?>" class="site-logo bg-blue">
             <img src="<?php get_home_url()?>/assets/src/svg/logo.svg" class="float-left mt-1" alt="Logo">
             <h1 class="float-left">Design system</h1>
         </a>
